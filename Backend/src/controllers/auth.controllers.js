5f46dd32a2690a5d4cb988c270b21ff2bc92956f
Tarun Pandav <tarunpandav5@gmail.com>
2026-07-25 18:39:12 +0530

    Backend

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -3,8 +3,75 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 async function regitserUser(req, res) {
+    const { username, email, password } = req.body;
+
+    const isAlreadyRegistered = await userModel.findOne({
+        $or:[
+            { email },
+            { username }
+        ]
+
+    })
+
+    if(isAlreadyRegistered){
+        return res.status(400).json({
+            message: "Us with the username is already exists" 
+        })
+    }
+
+    const hash = await bcrypt.hash(password, 10)
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash
+    })
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+
+    }, process.env.JWT_SECRET, 
+       {
+
+           expiresIn: "3d"
+           
+        }
+    )
+
+
+    res.cookie("token", token)
+
+    return res.status(201).json({
+        message: "User registered sucessfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
+
+        }
+    })
 
     
+}
+
+async function loginUser(rrq, res){
+    const { email , password, username } = req.body;
+
+
+    const user = await userModel.findOne({
+        $or: [
+            { email },
+            { username }
+        ]
+    })
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid credentials"
+        })
+
+
+    }
 }
 
 
